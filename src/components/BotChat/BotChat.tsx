a0d584b8728d860b3b013208d0731762dc03721f
gladosq <gladosq@gmail.com>
2023-08-16 11:43:19 +0400

    fin

--- src/components/BotChat/BotChat.tsx
+++ src/components/BotChat/BotChat.tsx
@@ -22,7 +22,7 @@ export default function BotChat() {
 
   const getBotMessageData = async (newMessage: string) => {
     const response = await axios.post(
-      `https://ai-chat-service.netlify.app/api/v1/chat/send-message`, {message: newMessage}
+      `http://185.46.8.130/api/v1/chat/send-message`, {message: newMessage}
     );
 
     const result = decodeToCorrectJson(response.data);
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  console.log('command:', command);
-
-  if (command === 'serve') {
-    return {
-      plugins: [react()],
-      define: {
-        __APP_ENV__: JSON.stringify(env.BASE_URl),
-      },
-    }
-  } else {
-    // command === 'build'
-    return {
-      plugins: [react()],
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
